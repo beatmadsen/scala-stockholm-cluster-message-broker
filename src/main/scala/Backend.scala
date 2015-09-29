abfe32e30a3dc500673366460a557812c4005ff9
emadsen <beatmadsen@gmail.com>
2015-09-29 22:31:03 +0200

    EM: Tests with seeds on one machine and nodes joining on another

--- src/main/scala/Backend.scala
+++ src/main/scala/Backend.scala
@@ -1,4 +1,6 @@
 import akka.actor._
+import akka.cluster.Cluster
+import akka.cluster.ClusterEvent._
 
 object Backend {
 
@@ -17,19 +19,36 @@ class Backend(host: String, port: Int) extends Actor with ActorLogging {
 
   val topicConnection = context.system.actorOf(TopicConnection.props("some-topic"))
 
-  self ! Started
-  topicConnection ! TopicConnection.Message("some message")     // TODO: Only do this when a new node comes on.
+  val cluster = Cluster(context.system)
 
 
-  def receive = {
+  // subscribe to cluster changes, re-subscribe when restart
+  override def preStart(): Unit = {
 
-    case Started ⇒
-      log.info("Backend started at {}:{}", host, port)
+    //#subscribe
+    cluster.subscribe(self, initialStateMode = InitialStateAsEvents,
+      classOf[MemberEvent], classOf[UnreachableMember])
+    //#subscribe
   }
 
 
   override def postStop(): Unit = {
 
     log.info("Backend at {}:{} stopping", host, port)
+    cluster.unsubscribe(self)
+  }
+
+
+  def receive = {
+
+    case MemberUp(member) =>
+      log.info("Member is Up: {}", member.address)
+      topicConnection ! TopicConnection.Message("some message")
+    case UnreachableMember(member) =>
+      log.info("Member detected as unreachable: {}", member)
+    case MemberRemoved(member, previousStatus) =>
+      log.info("Member is Removed: {} after {}",
+        member.address, previousStatus)
+    case _: MemberEvent => // ignore
   }
 }
