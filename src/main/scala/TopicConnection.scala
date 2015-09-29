c6b8f171e155a95c98e53b7c83522fc69de25a3b
emadsen <beatmadsen@gmail.com>
2015-09-29 20:48:27 +0200

    EM: Changes made during hands-on part of event

--- src/main/scala/TopicConnection.scala
+++ src/main/scala/TopicConnection.scala
@@ -1,6 +1,9 @@
 import java.util.UUID
 
 import akka.actor._
+import akka.cluster.client.ClusterClient.Publish
+import akka.cluster.pubsub.DistributedPubSub
+import akka.cluster.pubsub.DistributedPubSubMediator.{SubscribeAck, Subscribe}
 
 object TopicConnection {
 
@@ -25,15 +28,21 @@ class TopicConnection(topic: String) extends Actor with ActorLogging {
   log.info("Client session {} started", id.toString)
 
   // TODO subscribe to topic
+  val mediator = DistributedPubSub(context.system).mediator
+
+  mediator ! Subscribe(topic, self)
 
   def receive = {
     case Message(text) =>
       log.info("Got message from client {}: {}", id, text)
       // TODO publish to topic
+      mediator ! Publish(topic, DistributedMessage(id, text))
+
 
     case msg @ DistributedMessage(uuid, text) if uuid != id =>
       log.info("Got message from other cluster node: {}", msg)
       // TODO send to client
+      client ! Message(text)
 
 
     case OutgoingDestination(destination) =>
@@ -43,6 +52,9 @@ class TopicConnection(topic: String) extends Actor with ActorLogging {
     case Terminated(who) if who == client =>
       // if we cannot send messages, we might just as well shut down business
       context.stop(self)
+
+    case SubscribeAck(x) ⇒
+      log.info("subscribing: {}", x);
 
   }
 
